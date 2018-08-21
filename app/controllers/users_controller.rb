ec4204c101a7f57ea7ee58d53e756adeb713df26
JP Lynch <JPLynch35@gmail.com>
2018-08-21 14:09:28 -0600

    add feature tests and functionality for user viewing starred projects, number of followers and following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def show
     render file: 'public/404' unless current_user && current_user.id.to_s == params[:id]
+
+    starred_repos_reponse = conn.get('starred')
+    starred_repos = JSON.parse(starred_repos_reponse.body)
+    @starred_repos_count = starred_repos.count
+
+    followers_reponse = conn.get('followers')
+    followers = JSON.parse(followers_reponse.body)
+    @followers_count = followers.count
+
+    following_reponse = conn.get('following')
+    following = JSON.parse(following_reponse.body)
+    @following_count = following.count
   end
 end
--- spec/features/user_can_see_user_show_page_spec.rb
+++ spec/features/user_can_see_user_show_page_spec.rb
@@ -5,22 +5,19 @@ describe 'a logged in user ' do
     it 'can see their profile pic, name, number of starred repos, followers and followings' do
       user = User.create(
                           provider: 'github',
-                          uid: '32902349',
+                          uid: ENV['JP_UID'],
                           name: 'JP',
                           profile_pic: 'https://avatars0.githubusercontent.com/u/32905782?...',
-                          token: '0e23498239eb98238423eb'
+                          token: ENV['JP_TEST_TOKEN']
                         )
-      starred_repos_api_result = ['project1', 'project2', 'project3']
-      followers_api_result = ['user1', 'user2', 'user3', 'user4']
-      following_api_result = ['user1', 'user2']
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit user_path(user)
 
       expect(page).to have_content(user.name)
-      expect(page).to have_content("Number of Starred Repo: #{starred_repos_api_result.count}")
-      expect(page).to have_content("Number Followers: #{followers_api_result.count}")
-      expect(page).to have_content("Number Following: #{following_api_result.count}")
+      expect(page).to have_content("Number of Starred Repo: 0")
+      expect(page).to have_content("Number Followers: 1")
+      expect(page).to have_content("Number Following: 0")
     end
   end
 end
