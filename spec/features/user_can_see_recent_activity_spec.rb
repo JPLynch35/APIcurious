fe5d7edb47212eae32e6386f0a87a5e4c0ffd23c
JP Lynch <JPLynch35@gmail.com>
2018-08-21 20:20:24 -0600

    add feature test for recent activity page

--- spec/features/user_can_see_recent_activity_spec.rb
+++ spec/features/user_can_see_recent_activity_spec.rb
@@ -22,24 +22,22 @@ describe 'a logged in user ' do
     end
   end
 
-  # describe 'visiting the user recent activity page' do
-  #   it 'can see a summary feed of their recent activity (commits)' do
-  #     user = User.create(
-  #                         provider: 'github',
-  #                         uid: ENV['JP_UID'],
-  #                         name: 'JP',
-  #                         profile_pic: 'https://avatars0.githubusercontent.com/u/32905782?...',
-  #                         token: ENV['JP_TEST_TOKEN']
-  #                       )
-  #     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  describe 'visiting the user recent activity page' do
+    it 'can see a summary feed of their recent activity (commits)' do
+      user = User.create(
+                          provider: 'github',
+                          uid: ENV['JP_UID'],
+                          name: 'JP',
+                          profile_pic: 'https://avatars0.githubusercontent.com/u/32905782?...',
+                          token: ENV['JP_TEST_TOKEN']
+                        )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-  #     visit user_path(user)
+      visit user_recent_activity_path(user)
 
-      
-  #     expect(page).to have_content(user.name)
-  #     expect(page).to have_content("Number of Starred Repo: 0")
-  #     expect(page).to have_content("Number Followers: 1")
-  #     expect(page).to have_content("Number Following: 0")
-  #   end
-  # end
+      expect(page).to have_content('Repo: JPLynch35/APIcurious')
+      expect(page).to have_content('Time: 2018-08-22T01:17:36Z')
+      expect(page).to have_content('add feature test for repository list')
+    end
+  end
 end
