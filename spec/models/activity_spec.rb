6e7ecee91bb9f10b1fe25fb5c6eddeb7e5ece9c3
JP Lynch <JPLynch35@gmail.com>
2018-08-22 20:58:18 -0600

    add tests and functionality for following recent activity

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
-describe ProjectPush, type: :model do
+describe Activity, type: :model do
   it 'exists' do
-    data =  {
-              name: 'Dave'
-            }
-    activity = ProjectPush.new(data)
+    data =  [
+              'LittleTruck',
+              '2018-08-22T21:24:09Z',
+              ['refactored', 'refactored again']
+            ]
+    activity = Activity.new(data)
 
-    expect(activity).to be_a(Githubactivity)
+    expect(activity).to be_a(Activity)
   end
   it 'has project, time, and commit attributes' do
-    data = {login: 'Dave'}
-    activity = ProjectPush.new(data)
+    data =  [
+              'LittleTruck',
+              '2018-08-22T21:24:09Z',
+              ['refactored', 'refactored again']
+            ]
+    activity = Activity.new(data)
 
-    expect(activity.project).to eq('Dave')
-    expect(activity.time).to eq('Dave')
-    expect(activity.commits.count).to eq('Dave')
+    expect(activity.project).to eq('LittleTruck')
+    expect(activity.time).to eq('2018-08-22T21:24:09Z')
+    expect(activity.commits).to eq(['refactored', 'refactored again'])
   end
 end
